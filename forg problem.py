27835365344b498905cf77bbf2f6372b0d6a871e
gravy1333 <37875177+gravy1333@users.noreply.github.com>
2019-09-12 15:25:12 -0500

    Add files via upload

---
+++ forg problem.py
@@ -0,0 +1,20 @@
+import random
+while(1):
+    #dist = input("Distence: ")
+    #trials = input("trial number: ")
+    #dist = int(dist)
+    #trials = int(trials)
+    jumps = 0
+    dist = random.randint(35, 100)
+    trials = 1000000
+    print(dist)
+    
+    for tri in range(trials):
+        pos = 0
+        while(pos != dist):
+            jump = random.randint((pos+1), dist)
+            pos = jump
+            jumps += 1
+    
+    avg = jumps / trials
+    print(avg)
